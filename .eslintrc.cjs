52d193af6168554ebcc1d2844ae661ccc01445c2
jhoonyeel <jhoonyeel@gmail.com>
2025-06-07 05:42:59 +0900

    chore: ESLint env설정 수정
    node 환경 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,32 +3,33 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
   ],
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     // 필요시 커스터마이징 (예시)
-    "react/react-in-jsx-scope": "off", // Vite/React 17 이상에서는 필요 없음
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
+    'react/react-in-jsx-scope': 'off', // Vite/React 17 이상에서는 필요 없음
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
 };
