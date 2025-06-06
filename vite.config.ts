de67c0686897d56eb582eb42201f882a36e466be
jhoonyeel <jhoonyeel@gmail.com>
2025-06-07 05:44:24 +0900

    chore: emotion 초기 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: ['@emotion'],
+      },
+    }),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
