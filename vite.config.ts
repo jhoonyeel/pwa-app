516629a0196bc68e4310f64ca6c377f7d8953ecb
jhoonyeel <jhoonyeel@gmail.com>
2025-06-07 20:19:10 +0900

    chore: RTL 단위 테스트 환경 세팅
    Vitest 테스트 러너 타입 글로벌 선언

--- vite.config.ts
+++ vite.config.ts
@@ -34,4 +34,27 @@ export default defineConfig({
       },
     }),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    coverage: {
+      reporter: ['text', 'html'], // ✅ 커버리지 리포트 형식
+      provider: 'v8', // ✅ 커버리지 측정 도구
+      reportsDirectory: './coverage', // ✅ 결과 저장 위치
+      all: true, // 프로젝트 전체 커버리지 측정 (안 쓰면 실행된 파일만 측정)
+      include: ['src/**/*.{ts,tsx}'], // ✅ 커버리지 측정 대상
+      exclude: [
+        // 타입 선언 관련
+        'src/**/*.d.ts',
+
+        // 부트스트랩/환경 파일
+        'src/main.tsx',
+        'src/setupTests.ts',
+
+        // 테스트 파일
+        'src/**/*.test.{ts,tsx}',
+      ], // ✅ 제외 대상
+    },
+  },
 });
