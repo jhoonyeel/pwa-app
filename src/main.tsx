d5ef01530ebfdbe79e787ef3fed3e95f0ff79a22
jhoonyeel <jhoonyeel@gmail.com>
2025-06-08 18:17:23 +0900

    chore: 브라우저 환경 MSW 세팅
    public/ 서비스 워커 초기화, brower.ts 브라우저 환경 인터셉트 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// MSW dev 환경에서만 실행
+if (import.meta.env.DEV) {
+  const { worker } = await import('./mocks/browser.ts');
+  worker.start();
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
