8491dd9e02f5f6c22cbc7420629ecefcee0c5bfe
jhoonyeel <jhoonyeel@gmail.com>
2025-06-10 04:47:21 +0900

    chore: 배포환경에서의 MSW 임시 활성화 세팅
    VITE_ENV_MSW 환경변수 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-// MSW dev 환경에서만 실행
-if (import.meta.env.DEV) {
-  const { worker } = await import('./mocks/browser.ts');
-  worker.start();
+async function prepareMockWorker() {
+  // MSW dev 환경에서만 실행
+  if (import.meta.env.MODE === 'development' || import.meta.env.VITE_USE_MSW === 'true') {
+    const { worker } = await import('./mocks/browser');
+    await worker.start();
+
+    // MSW 활성화 로그 출력
+    console.log('%c[MSW] Mock Service Worker is running.', 'color: green; font-weight: bold;');
+  }
 }
+
+prepareMockWorker();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
