0b2e3c7d58827dfccd76073fc17f44c38a3f023e
jhoonyeel <jhoonyeel@gmail.com>
2025-06-10 05:22:14 +0900

    chore: 개발환경, 배포환경 핸들러 구분

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,13 +2,14 @@
 import { http, HttpResponse } from 'msw';
 import type { PetInfo } from '../types';
 
+const isDev = import.meta.env.DEV || import.meta.env.VITE_USE_MSW === 'true';
 const petData: PetInfo[] = [];
 
-export const handlers = [
+const devHandlers = [
   // POST /pet
   http.post('/pet', async ({ request }) => {
     const data = (await request.json()) as PetInfo; // 타입 단언 추가!
-    petData.push(data); // 쌓기
+    petData.push(data);
     console.log('POST /pet', data);
 
     return HttpResponse.json({}, { status: 201 });
@@ -33,3 +34,27 @@ export const handlers = [
     return HttpResponse.json({}, { status: 200 });
   }),
 ];
+
+const prodHandlers = [
+  http.post('*/pet', async ({ request }) => {
+    const data = (await request.json()) as PetInfo;
+    petData.push(data);
+    return HttpResponse.json({}, { status: 201 });
+  }),
+
+  http.get('*/pet', () => {
+    const latestData = petData[petData.length - 1];
+    if (latestData) {
+      return HttpResponse.json(latestData, { status: 200 });
+    } else {
+      return HttpResponse.json({ message: 'No data' }, { status: 404 });
+    }
+  }),
+
+  http.post('*/pet/reset', () => {
+    petData.length = 0;
+    return HttpResponse.json({}, { status: 200 });
+  }),
+];
+
+export const handlers = isDev ? devHandlers : prodHandlers;
